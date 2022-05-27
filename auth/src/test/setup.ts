b98c744ab9043170ec3b548fbfc8397059d272f5
kureyyh <k.dogan@student.fontys.nl>
2022-05-27 12:16:38 +0200

    update setup tests

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -9,41 +9,40 @@ declare global {
 
 let mongo: any;
 beforeAll(async () => {
-  process.env.JWT_KEY = "asdfasdf";
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+  process.env.JWT_KEY = "asdfsadf";
 
-  mongo = new MongoMemoryServer();
+  mongo = await MongoMemoryServer.create();
   const mongoUri = await mongo.getUri();
 
   await mongoose.connect(mongoUri);
+});
 
-  beforeEach(async () => {
-    const collections = await mongoose.connection.db.collections();
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
 
-    for (let collection of collections) {
-      await collection.deleteMany({});
-    }
-  });
-
-  afterAll(async () => {
-    await mongo.stop();
-    await mongoose.connection.close();
-  });
-
-  global.signin = async () => {
-    const email = "test@test.com";
-    const password = "password";
-
-    const response = await request(app)
-      .post("/api/users/signup")
-      .send({
-        email,
-        password,
-      })
-      .expect(201);
-
-    const cookie = response.get("Set-Cookie");
-
-    return cookie;
+  for (let collection of collections) {
+    await collection.deleteMany({});
   }
 });
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.signin = async () => {
+  const email = "test@test.com";
+  const password = "password";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+
+  return cookie;
+};
