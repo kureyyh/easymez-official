c243ad8e6f639aadab6ba10753a4c41b23ffaace
kureyyh <k.dogan@student.fontys.nl>
2022-05-27 12:36:15 +0200

    update setup tests

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,48 +1,51 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
-import request from "supertest";
 import { app } from "../app";
+import request from "supertest";
+
+let mongo: any;
 
 declare global {
   function signin(): Promise<string[]>;
 }
 
-let mongo: any;
-beforeAll(async () => {
-  process.env.JWT_KEY = "asdfsadf";
+beforeAll(async ()=>{
+  process.env.JWT_KEY = 'asdfsadf'
 
   mongo = await MongoMemoryServer.create();
-  const mongoUri = await mongo.getUri();
+  const mongoUri = mongo.getUri();
 
-  await mongoose.connect(mongoUri);
+  await mongoose.connect(mongoUri)
 });
 
 beforeEach(async () => {
   const collections = await mongoose.connection.db.collections();
 
-  for (let collection of collections) {
+  for(let collection of collections){
     await collection.deleteMany({});
   }
 });
 
-afterAll(async () => {
+afterAll(async ()=>{
   await mongo.stop();
   await mongoose.connection.close();
 });
 
 global.signin = async () => {
-  const email = "test@test.com";
-  const password = "password";
+    const email = 'test@test.com';
+    const password = 'password';
 
-  const response = await request(app)
-    .post("/api/users/signup")
-    .send({
-      email,
-      password,
-    })
-    .expect(201);
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email,
+            password
+        })
+        .expect(201);
 
-  const cookie = response.get("Set-Cookie");
-
-  return cookie;
+  return response.get('Set-Cookie');
 };
+
+
+
+
